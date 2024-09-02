9fddba6926458c1a9802af2d7b506e749aff35d7
suresh kumar singh <121798736+10Suresh@users.noreply.github.com>
2024-09-02 08:48:20 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import cors from "cors";
+// import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import express from "express";
@@ -8,17 +8,31 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MongoUri;
-const frontendURL = process.env.URL_FRONTEND;
-// Use CORS middleware
-app.use(
-  cors({
-    origin: frontendURL,
-    // origin:"http://localhost:3000",
-    // Remove the trailing slash from the origin URL
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  }),
-);
+// const frontendURL = process.env.URL_FRONTEND;
+// app.use(
+//   cors({
+//     origin: frontendURL,
+//     // origin:"http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true,
+//   }),
+// );
+const cors = require('cors');
+const corsOptions = {
+  origin: 'https://intern-frontend-drab.vercel.app', 
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
+const allowedOrigins = ['https://intern-frontend-drab.vercel.app'];
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Connect to MongoDB
 mongoose
   .connect(mongoURI, {
